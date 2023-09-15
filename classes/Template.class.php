da5b2260667f0f0f59ea279c91df59852669e83a
ahmaaher <ah.maaher@gmail.com>
2023-09-16 01:09:34 +0300

    Adding comments to Template.class.php

--- classes/Template.class.php
+++ classes/Template.class.php
@@ -2,22 +2,22 @@
 
 class Template{
 	
-	protected $temp_path;
-	protected $vars = array();
+	protected $temp_path; // path to template
+	protected $vars = array(); // the array that we'er gonna extract() our variables form
 
-	function __construct($temp_path){ $this->temp_path = $temp_path; }
+	function __construct($temp_path){ $this->temp_path = $temp_path; } // Setting the path to our template
 
-	//here if we tryed to assign new key value to the object, this to functions are for this work.
-	function __get($key){ return $this->vars[$key]; }
-	function __set($key, $val){ $this->vars[$key] = $val; }
+	//here if we tryed to assign new key value to the object, this two functions are for this work.
+	function __get($key){ return $this->vars[$key]; } // assigning array key
+	function __set($key, $val){ $this->vars[$key] = $val; } // assigning array value
 
-	function __toString(){
-		extract($this->vars);
-		ob_start();
-		include_once "temps/header.php";
+	function __toString(){  // __toString is called when we try to print our object => calling the object as string
+		extract($this->vars); // extract our variables from our array
+		ob_start(); // Strat output buffering
+		include_once "temps/header.php"; 
 		include_once $this->temp_path;
 		include_once "temps/footer.php";
-		return ob_get_clean();
+		return ob_get_clean(); // printing our buffered output and clean buffering
 	}
 }
 
